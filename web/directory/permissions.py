5e6b53d2cb6f12f6f125bc7cf4b07dd38a8883c4
Max Graziano <30026209+maxgraziano@users.noreply.github.com>
2024-05-15 12:08:33 -0500

    Removing errant print statements.

--- web/directory/permissions.py
+++ web/directory/permissions.py
@@ -2,6 +2,4 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        print(request)
-        print(obj)
         return bool(request.user and request.user.is_authenticated and (obj.user == request.user or request.user.is_staff))
